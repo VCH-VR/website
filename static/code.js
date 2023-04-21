720aaf716d2c050ab30ee30fc9e21ffcc40a9445
Undefined727 <33294766+Undefined727@users.noreply.github.com>
2023-04-21 16:43:27 -0700

    Create code.js

---
+++ static/code.js
@@ -0,0 +1,89 @@
+function hash(str) {
+    // Custom seed/"Salt" for hash function
+    let seed = 58511069;
+
+    // I'm gonna be honest idk what the rest of this function is I stole it off stack overflow
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for (let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+
+    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+}
+
+async function checkLogin() {
+    // Reads username and password and assigns them to a value
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    // Hashes password
+    password = hash(password);
+
+    // Calls the PASS function in /student as seen in index.py, assigns reply to variable data
+    const response = await fetch('https://undefined100.pythonanywhere.com/login', {
+        method: 'PASS',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username, password})});
+    return await response.text();
+}
+
+async function readLogin() {
+    answer = checkLogin();
+
+    answer.then(function(value) {
+        if (value.valueOf() == "correct") {
+        location.href = "patients";
+        }
+        else document.getElementById("message").innerHTML = value;
+    });
+}
+
+
+
+
+
+
+async function getPatients() {
+    const response = await fetch('https://undefined100.pythonanywhere.com/patientdata', {method: 'PULLLIST',});
+    return await response.text();
+}
+
+
+async function fillPatients() {
+    rawData = getPatients();
+
+    rawData.then(function(value) {
+        patientCount = (value.match(/{/g)||[]).length;
+        patientList = document.getElementById("patientList");
+
+        for (let i = 1; i <= patientCount; i++) {
+            substring = value.substring(value.indexOf("{")+1, value.indexOf("}"));
+            patientList.innerHTML += "<button onclick='display(" + i.toString() + ")'>" + substring + "</button>";
+            value = value.substring(value.indexOf("}")+1);
+        }
+    });
+}
+
+async function initializePatients() {
+    fillPatients();
+}
+
+async function initializeDisplay() {
+    // insert call to display data here
+}
+
+
+
+
+
+async function display(patientID) {
+    const response = await fetch('https://undefined100.pythonanywhere.com/display/' + patientID, {method: 'GET'});
+    location.href = "display/" + patientID;
+}
